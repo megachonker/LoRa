c220e5a4d1f64d128f4a7aaf539a2b334c938627
thirax <55107313+thirax@users.noreply.github.com>
2020-09-23 19:40:21 +0200

    Update receptionLora.py

--- recepteur/receptionLora.py
+++ recepteur/receptionLora.py
@@ -3,18 +3,40 @@ import socket # pour lire les message recus
 import time # pour la gestion des temps d'attente
 import os
 
+import struct
+from struct import *
+
+buffersize=64
+logtrames=[]
+
 lora = LoRa(mode=LoRa.LORA, region=LoRa.EU868, bandwidth=LoRa.BW_250KHZ, preamble=5, sf=8)
 s = socket.socket(socket.AF_LORA, socket.SOCK_RAW)
-s.setblocking(True)
 
-#fout =  open("imgOut.txt", "wb+")
+s.setblocking(True)
+nombretrames=s.recv(buffersize)
+s.settimeout(2)
+while trame=='':
+    s.send(nombretrames)
+    trame=s.recv(buffersize)
+logtrames.append(trame)
 print("démarage reception")
+#for notrame in range(nbtrame-1):
+s.setblocking(True)
+while True:
+    trame=s.recv(buffersize)
+    if trame=b'STOP':
+        s.send('oKay')
+        break
+    logtrames.append(trame)    
+
+unpack("B"+str(buffersize-1)+"s", trames)#on  concatène le no de trame est le numéro  de tram suivant + les  data
+
 
 data=b''
 compteur=0
 while True:
     chunk=s.recv(128)
-    print(chunk.read(1))
+    print(chunk[1])
     if(len(chunk)>0):
         compteur+=1
         print(compteur)
