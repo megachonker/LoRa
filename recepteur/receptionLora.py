ac99f3771a031a8dcfc427297c227c937b193875
thirax <55107313+thirax@users.noreply.github.com>
2020-09-24 20:09:17 +0200

    update
    pas fini >.<

--- recepteur/receptionLora.py
+++ recepteur/receptionLora.py
@@ -2,7 +2,6 @@ from network import LoRa #pour etre en mode LoRa
 import socket # pour lire les message recus
 import time # pour la gestion des temps d'attente
 import os
-
 import struct
 from struct import *
 
@@ -13,13 +12,45 @@ lora = LoRa(mode=LoRa.LORA, region=LoRa.EU868, bandwidth=LoRa.BW_250KHZ, preambl
 s = socket.socket(socket.AF_LORA, socket.SOCK_RAW)
 
 s.setblocking(True)
-nombretrames=s.recv(buffersize)
-s.settimeout(2)
-while trame=='':
+#s.settimeout(2)
+
+#définition d'une fonction d'aquitement
+def sendACK(var):
+	s.setblocking(False)
+	retour=s.recv(buffersize)
+	i=0
+	while len(retour)<0:
+		i+=1
+		print("essait d'envoit n° ",i)
+		s.send(var)
+		print("attente ack...")
+		retour=s.recv(buffersize)
+	print("ack Reçus")
+    s.setblocking(True)
+	return retour
+
+
+nbtrame=s.recv(buffersize)
+s.send("nombre de trame "+str(nbtrame))
+
+indexRecieve=[]
+print("démarage reception")
+
+
+while True:#trame!="STOP"
     s.send(nombretrames)
     trame=s.recv(buffersize)
+    if trame=="STOP":
+        print("fin de flux reçus  !")
+        break
+    else:
+        unpackted=unpack("B"+str(buffersize-1)+"s", trame)#on stoque la data qui est dans un  tuple dans une variable
+        #if(indexRecieve.index(unpackted))
+        indexRecieve.append(unpackted)
+        print(unpackted)
+
+
 logtrames.append(trame)
-print("démarage reception")
 #for notrame in range(nbtrame-1):
 s.setblocking(True)
 while True:
@@ -27,7 +58,7 @@ while True:
     if trame=b'STOP':
         s.send('oKay')
         break
-    logtrames.append(trame)    
+    logtrames.append(trame)
 
 unpack("B"+str(buffersize-1)+"s", trames)#on  concatène le no de trame est le numéro  de tram suivant + les  data
 
