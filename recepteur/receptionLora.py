3256904d9962418ae71c8ca4eecc902883e8b250
thirax <55107313+thirax@users.noreply.github.com>
2020-09-25 17:27:58 +0200

    fix indentation

--- recepteur/receptionLora.py
+++ recepteur/receptionLora.py
@@ -17,11 +17,11 @@ s.setblocking(True)
 #s.settimeout(2)
 
 def unboxing(rawtram)
-    global indexRecieve indexManque
-    unpackted=unpack("B"+str(buffersize-1)+"s", rawtram)#on stoque la data qui est dans un  tuple dans une variable
-    indexRecieve.append(unpackted)
-    indexManque.remove(unpackted[0])
-    print(unpackted)
+	global indexRecieve indexManque
+	unpackted=unpack("B"+str(buffersize-1)+"s", rawtram)#on stoque la data qui est dans un  tuple dans une variable
+	indexRecieve.append(unpackted)
+	indexManque.remove(unpackted[0])
+	print(unpackted)
 
 
 #définition d'une fonction d'aquitement
@@ -31,13 +31,13 @@ def sendACK(var):
 	i=0
 	while len(retour)<0:
 		i+=1
-        #temps attente random ?
+		#temps attente random ?
 		print("essait d'envoit n° ",i)
 		s.send(var)
 		print("attente ack...")
 		retour=s.recv(buffersize)
 	print("ack Reçus")
-    s.setblocking(True)
+	s.setblocking(True)
 	return retour
 
 
@@ -57,24 +57,23 @@ unboxing(sendACK("nombre de trame "+str(nbtrame)))
 print("démarage reception")
 
 while True:
-    while True:#trame!="STOP"
-        #s.send(nombretrames)
-        trame=s.recv(buffersize)
-        if trame=="STOP":
-            print("fin de flux reçus  !")
-            break
-        else:
-            unboxing(trame)
-    if(len(indexManque)==0):
-        print("plus de trame manquante.")
-        break
-
-    print("trame perdu/restant:")
-    print(indexManque)
-    #Envoit des trame a  retransmetre
-    #+ ajout de la premierre trame reçus (data)
-    unboxing(sendACK(indexManque))
-    print("début de la rerectption")
+	while True:#trame!="STOP"
+		#s.send(nombretrames)
+		trame=s.recv(buffersize)
+		if trame=="STOP":
+			print("fin de flux reçus  !")
+			break
+		else:
+			unboxing(trame)
+	if(len(indexManque)==0):
+		print("plus de trame manquante.")
+		break
+	 print("trame perdu/restant:")
+	print(indexManque)
+	#Envoit des trame a  retransmetre
+	#+ ajout de la premierre trame reçus (data)
+	unboxing(sendACK(indexManque))
+	print("début de la rerectption")
 
 
 print("récéption terminer:")
@@ -84,9 +83,9 @@ print(indexRecieve)
 print("écriture en cour:")
 
 with open('imgOut.txt', 'w') as fout: #création de du fichier data.txt sur le module si il n'est pas present, ou sinon on l'ouvre en mode ajout de data.
-    for truc in indexRecieve:
-        fout.write(truc[1]) # 3 saut à la ligne pour diferentier les data precedentes
-        print("chunk écrit:\n",str(truc[1]))
+	for truc in indexRecieve:
+		fout.write(truc[1]) # 3 saut à la ligne pour diferentier les data precedentes
+		print("chunk écrit:\n",str(truc[1]))
 fout.close() # on referme le fichier
 
 print("transfer  terminer")
