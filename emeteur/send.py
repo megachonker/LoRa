014bd6413544aac270acbd9686389828ebab504b
megachonker <55107313+megachonker@users.noreply.github.com>
2020-10-03 14:24:18 +0200

    pass1 ok
    code   debuguer pour faire la  passe 1

--- emeteur/send.py
+++ emeteur/send.py
@@ -10,32 +10,44 @@ buffersize=64 #taille  du  buffer  de récéption
 lora = LoRa(mode=LoRa.LORA, region=LoRa.EU868, bandwidth=LoRa.BW_250KHZ,preamble=5, sf=8)#définition dun truc
 s = socket.socket(socket.AF_LORA, socket.SOCK_RAW)#définition d'un socket réseaux de type lora
 f = open('img.py', 'rb')#on va ouvrire l'image qui port l'extention .py (pycom n'axepte pas  des fichier de format image)
-
+s.setblocking(True)
+s.settimeout(2)
 #définition d'une fonction d'aquitement
-def sendACK(var):
-	s.setblocking(False)
-	retour=s.recv(buffersize)
+def sendACK(vara):
 	i=0
-	while len(retour)<0:
+	while True:
 		i+=1
-		print("essait d'envoit n° ",i)
-		s.send(var)
+		s.send(str(vara))
 		print("attente ack...")
-		retour=s.recv(buffersize)
-	print("ack Reçus")
+		try:
+			retour=s.recv(buffersize)
+			print("ack Reçus")
+			break
+		except OSError as socket :
+			print("timeout try n° ",i)
 	return retour
 
 #nbtrame=(os.path.getsize('img.py')//buffersize-1)+1#nombre de  trame a envoyer
 
 #initialisation de la map de donnée
-dataMap=[]
-for nbChunk in range((os.path.getsize('img.py')//buffersize-1)+1)
-	print("maping de du chunk n°",nbChunk)
-	dataMap.append(f.read(buffersize-1)))
-	print(dataMap[nbChunk])
 
-print("array contenant les data maper:")
-print(dataMap)
+# totalChunk=(os.stat('img.py')[0]//(buffersize-1))+1
+# for nbChunk in range(totalChunk):
+# 	print("maping de du chunk n°",nbChunk)
+# 	dataMap.append(f.read(buffersize-32))
+# 	print(dataMap[nbChunk])
+
+
+dataMap=[]
+f = open('img.py', 'rb')
+var=b'e'
+while var!=b'':
+	var=f.read(buffersize-1)
+	dataMap.append(var)
+
+
+# print("array contenant les data maper:")
+# print(dataMap)
 
 ###initialisation d'un tableaux qui va lister tout les chunk de data
 #indexToSend[0,1,2,3,4,5,6,7,8,9]
@@ -45,17 +57,18 @@ for number in range(len(dataMap)):
 print("initial index tableaux sending intialiser !")
 print(indexToSend)
 #send du nombre de trame
-print("send demande de communiquation et annonce de ",nbtrame," trame a envoiller")
+print("send demande de communiquation et annonce de ",str(len(dataMap))," trame a envoiller")
+
 print(sendACK(len(dataMap)))#faire des rery avec des  ack
 
-echo("sucès début de transmition")
+print("sucès début de transmition")
 while len(indexToSend)!=0:
 	for notrame in range(len(indexToSend)):
 		#on map la trame en  utilisant un octée pour anoncer le nombre de tram est ensuite 63 suivant pour les data
-		trame=pack("B"+str(tl-1)+"s",notrame, dataMap[indexToSend[notrame]])
+		trame=pack("H"+str(buffersize-2)+"s",notrame, dataMap[indexToSend[notrame]])#buffersize = tl ?
 		#f.read(buffersize-1))#on  concatène le no de trame est le numéro  de tram suivant + les  data
 		s.send(trame) # envoie du message
-		print("trame n°"+str(notrame)+"envoiller")
+		print("trame n°"+str(notrame))
 	print("toute les trame on étée envoiler")
 	print("envoit de trame de fin")
 	missingTrame=sendACK("STOP")
